4f8db918adcfbec641f6ec57da01b27fa96e48ea
Siwanont Sittinam <siwanont1@gmail.com>
2020-06-04 22:54:37 +0700

    Add loop to check the Elasticsearch connection

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"io/ioutil"
+	"encoding/json"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/elastic/go-elasticsearch/v8"
@@ -11,21 +13,39 @@ import (
 
 func main() {
 	log.Print("Starting the Server")
-	r := gin.Default()
-	r.GET("/reviews", getReviewsByKeyword)
-	r.GET("/reviews/:id", getReviewsByID)
-	r.PUT("/reviews/:id", editReviewsByID)
-	r.Run()
+	router := gin.Default()
+	router.GET("/reviews", getReviewsByKeyword)
+	router.GET("/reviews/:id", getReviewsByID)
+	router.PUT("/reviews/:id", editReviewsByID)
+	router.Run()
 
 	log.Print("Starting the Database Server")
 	var (
 		r map[string]interface{}
 	)
 
-	elasticClient, err := elasticsearch.NewClient()
+	cfg := elasticsearch.Config{
+		Addresses: []string {},
+	}
+
+	elasticClient, err := elasticsearch.NewClient(cfg)
 	if err != nil {
 		log.Fatalf("Error creating the client: %s", err)
-	} 
+	}
+
+	for {
+		res, err := elasticClient.Info()
+		if err == nil {
+			json.NewDecoder(res.Body).Decode(&r)
+			log.Println("Connected to Elasticsearch")
+			res.Body.Close()
+			break
+		} else {
+			log.Println("Waiting for connection...")
+			time.Sleep(5 * time.Second)
+		}
+	}
+
 }
 
 func getReviewsByID (c *gin.Context) {
