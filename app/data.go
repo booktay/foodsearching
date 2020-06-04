29d7f08004534260b3d0112dc53645e33afe30a8
Siwanont Sittinam <siwanont1@gmail.com>
2020-06-04 23:46:41 +0700

    change variables' name

--- app/data.go
+++ app/data.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"flag"
+	"encoding/csv"
+	"log"
+	"os"
 )
 
-var reviewDirectory = flag.String("reviewDirectory", "data/test_file.csv", "Review Directory")
-var foodKeywordDirectory = flag.String("foodKeywordDirectory", "data/food_20k_keyword.txt", "Food Keyword Directory")
+var foodReviewsDir = flag.String("foodReviewsDir", "data/test_file.csv", "Food Reviews Directory")
+var foodKeywordDir = flag.String("foodKeywordDir", "data/food_20k_keyword.txt", "Food Keyword Directory")
 
 type FoodReview struct {
 	ID int `json:"reviewid"`
@@ -14,8 +17,7 @@ type FoodReview struct {
 	ModifiedTime int64 `json:"modified"`
 }
 
-type FoodDictionary struct {
+type FoodKeyword struct {
 	ID int `json:"keywordid"`
 	Keyword string `json:"keyword"`
 }
-
