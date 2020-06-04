cefa79174b952fb9692ab4ff8eccbc3081147728
Siwanont Sittinam <siwanont1@gmail.com>
2020-06-04 22:32:39 +0700

    Add Path editReviewsByID

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"io/ioutil"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,21 +11,36 @@ import (
 func main() {
 	log.Print("Starting the Server")
 	r := gin.Default()
-	r.GET("/reviews/:id", getReviewsByID)
 	r.GET("/reviews", getReviewsByKeyword)
+	r.GET("/reviews/:id", getReviewsByID)
+	r.PUT("/reviews/:id", editReviewsByID)
 	r.Run()
 }
 
 func getReviewsByID (c *gin.Context) {
 	reviewID := c.Param("id")
-	c.SecureJSON(http.StatusOK, gin.H{
-		"reviewID": reviewID,
-	})
+	if reviewID != "" {
+		c.SecureJSON(http.StatusOK, gin.H{
+			"reviewID": reviewID,
+		})
+	}
 }
 
 func getReviewsByKeyword (c *gin.Context) {
 	keyword := c.DefaultQuery("query", "")
-	c.SecureJSON(http.StatusOK, gin.H{
-		"Keyword": keyword,
-	})
+	if keyword != "" {
+		c.SecureJSON(http.StatusOK, gin.H{
+			"Keyword": keyword,
+		})
+	}
+}
+
+func editReviewsByID (c *gin.Context) {
+	body := c.Request.Body
+	if body != nil {
+		reviewText, _ := ioutil.ReadAll(body)
+		c.SecureJSON(http.StatusOK, gin.H{
+			"reviewText": string(reviewText),
+		})
+	}
 }
