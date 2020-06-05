7ffafe93bb066de7bcbd8ef122bd62d86b24e770
Siwanont Sittinam <siwanont1@gmail.com>
2020-06-06 05:41:03 +0700

    Add test

--- app/server_test.go
+++ app/server_test.go
@@ -4,58 +4,24 @@ import (
 	"testing"
 	"net/http"
 	"net/http/httptest"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/gin-gonic/gin"
 )
 
 func TestInput1IDOutDocument(t *testing.T) {
+	startElasticsearchConnection()
+	time.Sleep(10 * time.Second)
+
 	router := gin.Default()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/reviews/0", nil)
 	router.ServeHTTP(w, req)
 
-	template = `{
+	template := `{
 		"Message": "ReviewID is not found"
-	}`
-
-	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, template, w.Body.String())
-}
-
-func TestInput2IDOutDocument(t *testing.T) {
-	router := gin.Default()
-
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/reviews/6204", nil)
-	router.ServeHTTP(w, req)
-
-	template = `{
-		"Message": "ReviewID is not found"
-	}`
-
-	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, template, w.Body.String())
-}
-
-func TestInputIDInDocument(t *testing.T) {
-	router := gin.Default()
-
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/reviews/10", nil)
-	router.ServeHTTP(w, req)
-
-	template = `{
-		"_id": "10",
-		"_index": "reviews",
-		"_score": 7.81682,
-		"_source": {
-			"created": 1591378632031689000,
-			"modified": 1591385439459338000,
-			"reviewid": "10",
-			"reviewtext": "ดีใจที่จุดพักรถมอร์เตอร์เวย์ขาเข้า มีร้านอาหารจริงๆเสียที ขับรถมาเหนื่อยๆ อยากทานอาหารในร้านอาหารที่มีนั่งสบาย บริการแบบร้านอาหารจริงๆ ต้องที่นี่ค่ะ\nเมนูขึ้นชื่อของร้านมัลลิการ์ ก็ต้องเย็นตาโฟ ทั้งแบบใจเสาะ (เผ็ดปานกลาง) และรสเจ็บ (เผ็ดจริงจัง) แต่กว่าจะรู้สึกเผ็ดผ่านไปครึ่งชาม 555 รสเข้มข้น ไม่ต้องปรุงเลย เส้นใหญ่เหนียวนุ่ม ทานกับเต้าหู้ทอด หอม กรอบนอกนุ่มในอร่อยมากค่ะ"
-		},
-		"_type": "_doc"
 	}`
 
 	assert.Equal(t, 200, w.Code)
