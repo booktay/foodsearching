b43a98898b986882fd8dde8c08a12499dadcb69f
Siwanont Sittinam <siwanont1@gmail.com>
2020-06-06 16:12:11 +0700

    Add Unit Test TestInputResultFoodInDict function

--- app/elasticsearch_test.go
+++ app/elasticsearch_test.go
@@ -15,3 +15,23 @@ func TestInputDataloadReviewsAndKeyword(t *testing.T) {
 	assert.Equal(t, 6203, len(reviewDatas), "Equal")
 	assert.Equal(t, 20000, len(foodKeyword), "Equal")
 }
+
+func TestInputResultFoodInDict(t *testing.T) {
+	startElasticsearchConnection()
+
+	// resultEdit := editReviewsByMatchID("10", "")	
+	// test.AssertEquals(t, map[string]interface{}, reflect.TypeOf(resultEdit))
+	
+	resultFoodInDict := searchFoodInDictionary("assorted coffee")
+	resultFoodInDictMock := map[string]interface{} {
+		"_index": "foods",
+		"_type": "_doc",
+		"_id": "assorted coffee",
+		"_score": 1.0,
+		"_source": map[string]interface{} {
+			"keywordid": "19964",
+			"keyword": "assorted coffee",
+		},
+	}
+	assert.Equal(t, resultFoodInDictMock, resultFoodInDict, "Equal")
+}
