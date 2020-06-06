19626b4abb96bb79e11662fae29871a3f7d99f0b
Siwanont Sittinam <siwanont1@gmail.com>
2020-06-06 16:27:23 +0700

    Add Unit Test TestInputSearc by CorrectID and IncorrectID

--- app/elasticsearch_test.go
+++ app/elasticsearch_test.go
@@ -21,17 +21,89 @@ func TestInputResultFoodInDict(t *testing.T) {
 
 	// resultEdit := editReviewsByMatchID("10", "")	
 	// test.AssertEquals(t, map[string]interface{}, reflect.TypeOf(resultEdit))
-	
-	resultFoodInDict := searchFoodInDictionary("assorted coffee")
-	resultFoodInDictMock := map[string]interface{} {
-		"_index": "foods",
-		"_type": "_doc",
-		"_id": "assorted coffee",
-		"_score": 1.0,
-		"_source": map[string]interface{} {
-			"keywordid": "19964",
-			"keyword": "assorted coffee",
+	input := "assorted coffee"
+	foodInDict := searchFoodInDictionary(input)
+	results := foodInDict["_id"].(string)
+	assert.Equal(t, input, results, "Equal")
+}
+
+func TestInputSearchCorrectID(t *testing.T) {
+	startElasticsearchConnection()
+
+	testCases := [] struct {
+		Input string
+		Output map[string]interface{}
+	} {
+		{
+			Input: "1",
+			Output: map[string]interface{} {
+				"_id": "1",
+				"_index": "reviews",
+				"_score": 8.327645,
+				"_type": "_doc",
+			},
+		},
+		{
+			Input: "6203",
+			Output: map[string]interface{} {
+				"_id": "6203",
+				"_index": "reviews",
+				"_score": 8.327645,
+				"_type": "_doc",
+			},
 		},
 	}
-	assert.Equal(t, resultFoodInDictMock, resultFoodInDict, "Equal")
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.Input, func(t *testing.T) {
+			t.Parallel()
+			resultID := searchByMatchID(tc.Input)
+			delete(resultID, "_source")
+			assert.Equal(t, resultID, tc.Output)
+		})
+	}
+}
+
+func TestInputSearchInCorrectID(t *testing.T) {
+	startElasticsearchConnection()
+
+	testCases := [] struct {
+		Input string
+		Output map[string]interface{}
+	} {
+		{
+			Input: "0",
+			Output: map[string]interface{} {
+				"Message": "ReviewID is not found",
+			},
+		},
+		{
+			Input: "1 or 1=1",
+			Output: map[string]interface{} {
+				"Message": "ReviewID is not found",
+			},
+		},
+		{
+			Input: "Helloworld 1",
+			Output: map[string]interface{} {
+				"Message": "ReviewID is not found",
+			},
+		},
+		{
+			Input: "Helloworld",
+			Output: map[string]interface{} {
+				"Message": "ReviewID is not found",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.Input, func(t *testing.T) {
+			t.Parallel()
+			resultID := searchByMatchID(tc.Input)
+			assert.Equal(t, resultID, tc.Output)
+		})
+	}
 }
