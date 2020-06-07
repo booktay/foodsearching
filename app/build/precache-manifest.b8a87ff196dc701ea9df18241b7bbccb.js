6b0b2409335c534250992995598bfa935803f60b
Siwanont Sittinam <siwanont1@gmail.com>
2020-06-07 22:56:37 +0700

    Add Static Web UI at /app/build path

---
+++ app/build/precache-manifest.b8a87ff196dc701ea9df18241b7bbccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba326a310b92209d76509adbf0408484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c9c1b8b15accf94d7ef",
+    "url": "/static/css/main.7d9103e4.chunk.css"
+  },
+  {
+    "revision": "1a0d36df7507eafb2d99",
+    "url": "/static/js/2.fb11eea7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fb11eea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c9c1b8b15accf94d7ef",
+    "url": "/static/js/main.42884c5d.chunk.js"
+  },
+  {
+    "revision": "a2e189ab0fe3e684907a",
+    "url": "/static/js/runtime-main.53fb7169.js"
+  }
+]);
