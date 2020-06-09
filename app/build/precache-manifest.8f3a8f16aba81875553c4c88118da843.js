f41cb2524c546b19eca0822de4d611a72285bae7
Siwanont Sittinam <siwanont1@gmail.com>
2020-06-09 22:57:44 +0700

    Update static web ui

--- app/build/precache-manifest.8f3a8f16aba81875553c4c88118da843.js
+++ app/build/precache-manifest.8f3a8f16aba81875553c4c88118da843.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0145da64e374f6da642c04f3a1cc85d2",
+    "revision": "a4203aa2cf5d323f8401430154d88502",
     "url": "/index.html"
   },
   {
-    "revision": "5d8d71ef69936669a1fe",
+    "revision": "80ab687d56dd0a3df0d7",
     "url": "/static/css/main.7d9103e4.chunk.css"
   },
   {
-    "revision": "1a0d36df7507eafb2d99",
-    "url": "/static/js/2.fb11eea7.chunk.js"
+    "revision": "0cf289818c5cba7e2592",
+    "url": "/static/js/2.615704ba.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/static/js/2.fb11eea7.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.615704ba.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5d8d71ef69936669a1fe",
-    "url": "/static/js/main.0eb7b413.chunk.js"
+    "revision": "80ab687d56dd0a3df0d7",
+    "url": "/static/js/main.1737e04f.chunk.js"
   },
   {
     "revision": "a2e189ab0fe3e684907a",
--- app/build/service-worker.js
+++ app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.462858a4e54c14699afd824c5cff56b3.js"
+  "/precache-manifest.8f3a8f16aba81875553c4c88118da843.js"
 );
 
 self.addEventListener('message', (event) => {
